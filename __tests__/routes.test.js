7e2a16c38b72159bec8a997008fc26adfca96aab
Anthony Louis Morton <anthonymorton760@gmail.com>
2021-12-22 21:49:57 -0800

    some other changes

--- __tests__/routes.test.js
+++ __tests__/routes.test.js
@@ -26,36 +26,6 @@ describe('Testing our REST methods', () => {
     expect(response.body.type).toBe('junkFood');
   })
 
-  // it('should fetch all Foods on GET /api/v1/food', async () => {
-  //   const response = await request.get('/api/v1/food');
-  //   expect(response.status).toBe(200);
-  //   expect(response.body.length).toBe(1);
-  // })
-
-  // it('should fetch one Food on GET /api/v1/food:id', async () => {
-  //   const response = await request.get('/api/v1/food?id=1');
-  //   expect(response.status).toBe(200);
-  //   expect(response.body.length).toBe(1);
-  // })
-
-  // it('should be able to update a Food', async () => {
-  //   const response = await request.put('/api/v1/food/1').send({
-  //     name: 'Pizza', 
-  //     calories: '1000',
-  //     type: 'junkFood',
-  //   });
-
-  //   expect(response.body.name).toBe('Pizza');
-  //   expect(response.body.calories).toBe('1000');
-  //   expect(response.body.type).toBe('junkFood');
-
-  // })
-
-  // it('should delete a record', async () => {
-  //   const response = await request.delete('/api/v1/food/1');
-  //   expect(response.status).toBe(200)
-
-  // })
 
 })
 
